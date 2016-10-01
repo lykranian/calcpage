29523bb1b4862ca51327e4f810f6f1f5d993e10b
lyk <lyk@rectilinear.xyz>
2016-09-30 17:34:34 -0700

    populate

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# calcpage
+## calcpage
 
 php simple calculator script
+
+examples:
+
+```php
+calcpage.php?exp=1+2
+calcpage.php?exp=(4*4)/(3(4x2))
+```
---
+++ calcpage.php
@@ -0,0 +1,44 @@
+<?php
+
+// requires from https://github.com/andig/php-shunting-yard
+require_once "RR/Shunt/Context.php";
+require_once "RR/Shunt/Parser.php";
+require_once "RR/Shunt/Scanner.php";
+require_once "RR/Shunt/Token.php";
+require_once "RR/Shunt/Exception/ParseError.php";
+require_once "RR/Shunt/Exception/RuntimeError.php";
+require_once "RR/Shunt/Exception/SyntaxError.php";
+use RR\Shunt\Parser;
+
+// allows for + in the url, rather than %2B
+if(strpos($_SERVER["QUERY_STRING"], "+") !== false){
+   $_SERVER["QUERY_STRING"] = str_replace("+", "%2B", $_SERVER["QUERY_STRING"]);
+   parse_str($_SERVER["QUERY_STRING"], $_GET);
+}
+
+$exp_raw = trim($_GET['exp']);
+if(empty($exp_raw)){
+   echo 'empty input';
+   exit;
+} else {
+   $check = preg_match('#[^0-9eE()\+-.\/\*x\ ]#',$exp_raw);
+   if($check) {
+      $fail_char = preg_replace('#[0-9eE()\+-.\/\*x\ ]#','',$exp_raw);
+      echo "error, ";
+      echo $fail_char;
+      echo " is not a valid input character.";
+      exit;
+   } else {
+      // sanitize
+      $exp_san = preg_replace('#[^0-9eE()\+-.\/\*x\ ]#','',$exp_raw);
+      // catch a couple off-cases
+      $exp_par = str_replace(')(',')*(',$exp_san);
+      $exp_fix = str_replace('x','*',$exp_par);
+      // following two lines for debug
+      //echo $exp_fix;
+      //echo "\n";
+      $result = Parser::parse($exp_fix);
+      echo $result;
+   }
+}
+
